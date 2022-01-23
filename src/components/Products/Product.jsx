65202be6c99bbbeda425327df1ce830bdeda74a9
dipanc1 <dipanchhabra@gmail.com>
2022-01-23 15:42:54 +0530

    fetching it from api

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,38 +1,49 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Product.css';
 
 const Product = () => {
 
+    const [products, setProducts] = React.useState([]);
 
     const apiUrl = 'https://dnc0cmt2n557n.cloudfront.net/products.json';
-    axios.options(data => {
-        
-    })
+    useEffect(() => {
+        axios.get(apiUrl)
+            .then(response => {
+                console.log(response.data);
+                setProducts(response.data.products);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
 
-    return (
-        <div className="product">
-            <div className="product-image">
-                <img src="img.jpg" alt="product" />
+    return (<>
+        {products.map(product => (
+            <div className="product" key={product.id}>
+                <div className="product-image">
+                    <img src={product.image} alt="product" className='image'/>
+                </div>
+                <div className="product-details" >
+                    <div className="product-title">
+                        <h2>
+                            {product.title}
+                        </h2>
+                        <p>{product.desc}</p>
+                    </div>
+                    <div className="product-quantity">
+                        <h2 className='fab'>-</h2>
+                        <input type="number" min="1" max="9999" className='input' />
+                        <h2 className='fab'>+</h2>
+                    </div>
+                    <div className="product-price">
+                        <p>{product.currency}</p>
+                        <p>{product.price}</p>
+                    </div>
+                </div>
             </div>
-            <div className="product-details">
-                <div className="product-title">
-                    <h2>
-                        Product Title
-                    </h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla laudantium commodi eligendi?</p>
-                </div>
-                <div className="product-quantity">
-                    <h2 className='fab'>-</h2>
-                    <input type="number" min="1" max="9999" className='input'/>
-                    <h2 className='fab'>+</h2>
-                </div>
-                <div className="product-price">
-                    $19.99
-                </div>
-            </div>
-        </div>
-
+        ))}
+    </>
     );
 };
 
